7e903e97e26d7c6e006c2c92340f1992a178b502
Abdulganiyu Abubakar <abdulganiyua@hardcorebiometric.com>
2023-10-25 13:36:07 +0100

    Added kafka topic

---
+++ src/main/java/com/gsuretech/kafka/config/KafkaTopicConfig.java
@@ -0,0 +1,16 @@
+package com.gsuretech.kafka.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic gsuretechTopic(){
+        return TopicBuilder.name("gsuretech")
+                .build();
+    }
+}
