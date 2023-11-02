ccd4b731f6dbae4c79c695b6ffc3c1ca0c7de641
Abdulganiyu Abubakar <abdulganiyua@hardcorebiometric.com>
2023-11-02 08:32:32 +0100

    refactored digit

--- src/main/java/DigitAnagramPairs.java
+++ src/main/java/DigitAnagramPairs.java
@@ -4,18 +4,18 @@ import java.util.Map;
 
 public class DigitAnagramPairs {
 
-    public static int countDigitAnagramPairs(int[] a) {
-        Map<String, Integer> digitFrequencyMap = new HashMap<>();
+    public static int countDigitAnagram(int[] a) {
+        Map<String, Integer> digitFrequency = new HashMap<>();
         int count = 0;
 
         for (int num : a) {
             String sortedDigits = sortDigits(num);
 
-            if (digitFrequencyMap.containsKey(sortedDigits)) {
-                count += digitFrequencyMap.get(sortedDigits);
-                digitFrequencyMap.put(sortedDigits, digitFrequencyMap.get(sortedDigits) + 1);
+            if (digitFrequency.containsKey(sortedDigits)) {
+                count += digitFrequency.get(sortedDigits);
+                digitFrequency.put(sortedDigits, digitFrequency.get(sortedDigits) + 1);
             } else {
-                digitFrequencyMap.put(sortedDigits, 1);
+                digitFrequency.put(sortedDigits, 1);
             }
         }
 
@@ -30,7 +30,7 @@ public class DigitAnagramPairs {
 
     public static void main(String[] args) {
         int[] array = {123, 231, 312, 421, 144, 244};
-        int result = countDigitAnagramPairs(array);
+        int result = countDigitAnagram(array);
         System.out.println("Number of digit anagram pairs: " + result);
     }
 }
