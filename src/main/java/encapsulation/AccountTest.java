7b2dfe66f8deeb1800a313b3f51abece009b0200
Abdulganiyu Abubakar <abdulganiyua@hardcorebiometric.com>
2023-11-17 23:10:36 +0100

    account class created and initiated

---
+++ src/main/java/encapsulation/AccountTest.java
@@ -0,0 +1,12 @@
+package encapsulation;
+
+public class AccountTest {
+        public static void main(String[] args) {
+                Account account = new Account();
+                System.out.println(account);
+                account.firstName = "Abdulganiyu";
+                account.lastName = "Abubakar";
+                account.identityNumber = "S3330737933";
+        }
+
+}
