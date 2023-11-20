41a6caf0dc08368fc857ee727b558a960a64f9a2
Abdulganiyu Abubakar <abdulganiyua@hardcorebiometric.com>
2023-11-20 20:26:07 +0100

    added getters and setters

--- src/main/java/encapsulation/Account.java
+++ src/main/java/encapsulation/Account.java
@@ -7,7 +7,33 @@ public class Account {
     //private is only accessible from within the class it is declared
     private  String firstName;
     private String lastName;
-
     private String identityNumber;
 
+    //Mutator methods or setter methods to provide values for our instance variable
+    //Accessor methods or Getter methods to fetch the value assigned to our instance variable
+
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getIdentityNumber() {
+        return identityNumber;
+    }
+
+    public void setIdentityNumber(String identityNumber) {
+        this.identityNumber = identityNumber;
+    }
 }
