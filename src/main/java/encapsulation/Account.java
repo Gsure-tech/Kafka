518c4254a6148acbf26638aea015da4f664236d5
Abdulganiyu Abubakar <abdulganiyua@hardcorebiometric.com>
2023-11-20 20:16:19 +0100

    made the variables private

--- src/main/java/encapsulation/Account.java
+++ src/main/java/encapsulation/Account.java
@@ -1,10 +1,13 @@
 package encapsulation;
 
 public class Account {
-    //Instant variables
-    public String firstName;
-    public String lastName;
+    //Instance variables
+    //Encapsulation: Hide direct access to your instance variables
+    //provide access to them via public methods
+    //private is only accessible from within the class it is declared
+    private  String firstName;
+    private String lastName;
 
-    public String identityNumber;
+    private String identityNumber;
 
 }
