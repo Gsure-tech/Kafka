ada7b8d6ec9f901cf5dc70399f45c707ca8061d4
Abdulganiyu Abubakar <abdulganiyua@hardcorebiometric.com>
2023-11-22 16:53:30 +0100

    created second account

--- src/main/java/encapsulation/AccountTest.java
+++ src/main/java/encapsulation/AccountTest.java
@@ -1,5 +1,8 @@
 package encapsulation;
 
+import java.util.Scanner;
+import java.util.UUID;
+
 public class AccountTest {
         public static void main(String[] args) {
                 Account account = new Account();
@@ -8,7 +11,20 @@ public class AccountTest {
                 account.setLastName("Abubakar");
                 account.setIdentityNumber("23hdjjj3ss");
 
-                System.out.printf("Firstnamae:  %s\nLastname:%s\nIdentityNumber: %s", account.getFirstName(),account.getLastName(),account.getIdentityNumber());
+               // System.out.printf("Firstnamae:  %s\nLastname:%s\nIdentityNumber: %s", account.getFirstName(),account.getLastName(),account.getIdentityNumber());
+
+                Account account1 = new Account();
+                Scanner sc = new Scanner(System.in);
+                System.out.println( "Enter your Firstname");
+                String firstname = sc.nextLine();
+                account1.setFirstName(firstname);
+
+                System.out.println("Enter your Lastname");
+                String lastname = sc.nextLine();
+                account1.setLastName(lastname);
+
+                account.setIdentityNumber(UUID.randomUUID().toString());
+                System.out.println("Firstname: " + account1.getFirstName());
 
         }
 
